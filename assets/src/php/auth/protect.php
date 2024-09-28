4c23a8f8e7a4b47294ee946250b3003bafcfaad8
Rubens Lira <rubenslira1156@gmail.com>
2024-09-28 14:08:10 -0300

    1.1 Correção no arquivo protect.php

--- assets/src/php/auth/protect.php
+++ assets/src/php/auth/protect.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (empty($_SESSION["id"])) {
-    $redirectUrl = ($_SERVER["REQUEST_URI"] === "/Teste/") ? "assets/src/pages/login.php" : "login.php";
+    $redirectUrl = ($_SERVER["REQUEST_URI"] === "/CRUD/") ? "assets/src/pages/login.php" : "login.php";
     header("Location: $redirectUrl");
     exit();
 }
